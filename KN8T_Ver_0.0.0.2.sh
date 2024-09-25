92d79258d41153a411e07638d7ed145aaddd51c8
KN8T <69385743+BKN8T@users.noreply.github.com>
2024-09-26 00:42:07 +0300

    İconApp

--- KN8T_Ver_0.0.0.2.sh
+++ KN8T_Ver_0.0.0.2.sh
@@ -21,7 +21,7 @@
     # AppleScript'i uygulama olarak masaüstüne kaydet
         osascript -e 'tell application "Finder" to make new folder at desktop with properties {name:"KN8Tool"}'
         osascript -e 'tell application "Finder" to open folder "KN8Tool" of desktop'
-        osacompile -o ~/Desktop/MyScriptApp/MyScriptApp.app /tmp/temp_script.applescript
+        osacompile -o ~/Desktop/KN8Tool/KN8Tool.app /tmp/temp_script.applescript
 
         echo "macOS'ta masaüstüne kısayol oluşturuldu!"
 
@@ -30,7 +30,7 @@
         echo "Linux tespit edildi. Kısayol oluşturuluyor..."
 
     # Masaüstüne kısayol dosyası oluştur
-        DESKTOP_FILE="$HOME/Desktop/myscript.desktop"
+        DESKTOP_FILE="$HOME/Desktop/KN8Tool.desktop"
 
     # .desktop dosyası oluştur ve içine kısayol bilgilerini yaz
         echo "[Desktop Entry]
