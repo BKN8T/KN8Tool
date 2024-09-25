641d8591813140a53ecd171360de1e4734fea5b6
KN8T <69385743+BKN8T@users.noreply.github.com>
2024-09-26 00:38:40 +0300

    İconApp

--- KN8T_Ver_0.0.0.2.sh
+++ KN8T_Ver_0.0.0.2.sh
@@ -1,5 +1,56 @@
     #! /bin/bash 
 
+    #? İşletim sistemini tespit et
+    OS=$(uname)
+
+    #? Script'in tam yolunu bul (boşlukları desteklemek için)
+    SCRIPT_PATH="$(pwd)/$(basename "$0")"
+
+    #? macOS için otomatik kısayol oluşturma
+    if [[ "$OS" == "Darwin" ]]; then
+        echo "macOS tespit edildi. Kısayol oluşturuluyor..."
+
+    # AppleScript komutunu içeren bir geçici dosya oluştur
+        APPLESCRIPT="
+        do shell script \"/bin/bash '$SCRIPT_PATH'\"
+        "
+    
+    # AppleScript dosyasını geçici bir yere yaz
+        echo "$APPLESCRIPT" > /tmp/temp_script.applescript
+
+    # AppleScript'i uygulama olarak masaüstüne kaydet
+        osascript -e 'tell application "Finder" to make new folder at desktop with properties {name:"KN8Tool"}'
+        osascript -e 'tell application "Finder" to open folder "KN8Tool" of desktop'
+        osacompile -o ~/Desktop/MyScriptApp/MyScriptApp.app /tmp/temp_script.applescript
+
+        echo "macOS'ta masaüstüne kısayol oluşturuldu!"
+
+    # Linux için otomatik kısayol oluşturma
+    elif [[ "$OS" == "Linux" ]]; then
+        echo "Linux tespit edildi. Kısayol oluşturuluyor..."
+
+    # Masaüstüne kısayol dosyası oluştur
+        DESKTOP_FILE="$HOME/Desktop/myscript.desktop"
+
+    # .desktop dosyası oluştur ve içine kısayol bilgilerini yaz
+        echo "[Desktop Entry]
+        Version=0.0.0.2
+        Name=KN8Tool
+        Comment=KN8T Tool Kısa Yolu
+        Exec=\"$SCRIPT_PATH\"
+        Icon=utilities-terminal
+        Terminal=true
+        Type=Application" > "$DESKTOP_FILE"
+
+        # Kısayolu çalıştırılabilir yap
+        chmod +x "$DESKTOP_FILE"
+
+        echo "Linux'ta masaüstüne kısayol oluşturuldu!"
+    else
+        echo "Bu işletim sistemi desteklenmiyor."
+    fi
+
+
     #--------------------------------
     VERSION="0.0.0.2"
 
